560e338f61b6f3a0a6a596a041aef3e3ece6cde2
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-06-18 08:03:02 +0200

    created login page with form that retrieves a token by sending a POST request and stores it in LocalStorage

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,7 +6,66 @@ interface User {
 }
 
 const Login = ({ id }: User) => {
-  return <div className="login-page"></div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    const tokenFromStorage = localStorage.getItem("token");
+    if (tokenFromStorage !== null) {
+      router.push("/");
+    }
+  }, [router]);
+
+  const [userName, setUserName] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  const onSubmitTheForm = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    const result = await fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const json = await result.json();
+    if (json.token) {
+      localStorage.setItem("token", json.token);
+      router.push("/"); // ${id}??
+    }
+  };
+
+  return (
+    <div className="login-page">
+      <form className="login-window" onSubmit={onSubmitTheForm}>
+        <h1>Login</h1>
+        <label>Username</label>
+        <input
+          type="text"
+          required={true}
+          onChange={(event) => setUserName(event.target.value)}
+          value={userName}
+          className="input"
+          name="username"
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          required={true}
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          className="input"
+          name="password"
+        />
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
