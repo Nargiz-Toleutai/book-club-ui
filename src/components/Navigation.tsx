32ee5701d492f702ed94ae8c07102c44792d9462
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-06-18 08:57:48 +0200

    created Navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const Navigation = () => {
+  const [getToken, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const tokenFromStorage = localStorage.getItem("token");
+    setToken(tokenFromStorage);
+  }, []);
+
+  return (
+    <ul className="navigation-container">
+      <h1>Book club</h1>
+      <li className="navigation-item">
+        <Link href="/">Home</Link>
+      </li>
+      {getToken && (
+        <li className="navigation-item">
+          <Link href="/progress">Progress</Link>
+        </li>
+      )}
+      {getToken === null ? (
+        <>
+          <li className="navigation-item">
+            <Link href="/login">Login</Link>
+          </li>
+          <li className="navigation-item">
+            <Link href="/register">Registration</Link>
+          </li>
+        </>
+      ) : (
+        <li className="navigation-item">
+          <button
+            onClick={() => {
+              setToken(null);
+              localStorage.removeItem("token");
+            }}
+          >
+            Log out
+          </button>
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default Navigation;
